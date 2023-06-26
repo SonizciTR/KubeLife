712ff242c78c776fb738657dc16721734bdb56e4
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2023-06-26 13:52:18 +0300

    cronjob model remodelling

--- src/KubeCronMonitor.Kubernetes/Models/KubeCronJobModel.cs
+++ src/KubeCronMonitor.Kubernetes/Models/KubeCronJobModel.cs
@@ -12,12 +12,33 @@ namespace KubeCronMonitor.Kubernetes.Models
         public string CronJobName { get; set; }
         public string TimingRaw { get; set; }
         public bool IsSuspended { get; internal set; }
-        public DateTime? LastStartTime { get; internal set; }
-        public DateTime? LastEndTime { get; internal set; }
+
         public bool IsJobDetailSet { get; internal set; } = false;
         public List<KubeJobModel> JobDetails { get; internal set; } = new List<KubeJobModel>();
 
-        //Calculated Properties
+        #region Calculated Properties
+        private DateTime? _lastStartTime;
+        public DateTime? LastStartTime
+        {
+            get
+            {
+                if (!IsJobDetailSet) return _lastStartTime;
+                return JobDetails.Max(x => x.StartTime);
+            }
+            internal set => _lastStartTime = value;
+        }
+
+        private DateTime? _lastEndTime;
+        public DateTime? LastEndTime
+        {
+            get
+            {
+                if (!IsJobDetailSet) return _lastEndTime;
+                return JobDetails.Max(x => x.EndTime);
+            }
+            internal set => _lastEndTime = value;
+        }
+
         public double AvgRunTimeMinutes
         {
             get
@@ -50,5 +71,7 @@ namespace KubeCronMonitor.Kubernetes.Models
                 return JobDetails.Count(x => !x.IsSuccess);
             }
         }
+
+        #endregion
     }
 }
