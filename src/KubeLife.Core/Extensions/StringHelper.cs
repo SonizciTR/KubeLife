b2d0ede1d83065ab218786f1ae473bf95c58961b
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2023-07-05 12:54:39 +0300

    Log view frontend implemented

---
+++ src/KubeLife.Core/Extensions/StringHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KubeLife.Core.Extensions
+{
+    public static class StringHelper
+    {
+        public static string ToHtml(this string source)
+        {
+            return source.Replace("\n", "<br/>");
+        }
+    }
+}
