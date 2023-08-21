e9c8f5af548d8a42437c87e0e73e5b8415c7dfc5
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2023-08-21 15:37:46 +0300

    domain unit test preparation

---
+++ unittests/KubeLife.Domain.Tests/Fakes/FakeKubeService.cs
@@ -0,0 +1,59 @@
+﻿using KubeLife.Core.Models;
+using KubeLife.Kubernetes;
+using KubeLife.Kubernetes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KubeLife.Domain.Tests.Fakes
+{
+    internal class FakeSuccessEmptyKubeService : IKubeService
+    {
+        public async Task<KubeLifeResult<List<KubeRouteModel>>> GetAllRoutes(int routeCount = 500, string filterbyLabel = null)
+        {
+            return new KubeLifeResult<List<KubeRouteModel>>(true, "Unit test error");
+        }
+
+        public Task<List<KubeServiceModel>> GetAllServices(string namespaceParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<KubeCronJobModel>> GetCronJobs(string filterbyLabel = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<KubeJobModel>> GetJobsbyNamespace(string kubeNamespace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetLogofPod(string kubeNamespace, string podName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<KubePodModel>> GetPodsofNamespace(string kubeNamespace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<KubePodModel>> GetPodsOfService(KubeServiceModel serviceInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<KubeServiceModel> GetServiceOfRoute(string namespaceParam, string routeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<KubeLifeResult<KubeBuildModel>> TriggerBuildConfig(string namespaceParameter, string buildConfigName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- unittests/KubeLife.Domain.Tests/KubernetesDomainTest.cs
+++ unittests/KubeLife.Domain.Tests/KubernetesDomainTest.cs
@@ -1,11 +1,28 @@
+using AutoMapper;
+using KubeLife.Domain.Tests.Fakes;
+
 namespace KubeLife.Domain.Tests
 {
     public class KubernetesDomainTest
     {
+        private IKubernetesDomain GetSerivce()
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            return new KubernetesDomain(new FakeKubeService(), mapper);
+        }
+
         [Fact]
         public void Test1()
         {
+            var service = GetSerivce();
 
+            Assert.True(service != null);
         }
     }
 }
