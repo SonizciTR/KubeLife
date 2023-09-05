9c11c4c3bbce2dd86d810362e3d6788c8f44a71a
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2023-09-05 08:42:22 +0300

    BuildImageShow componenet added

---
+++ src/KubeLife.BlazorApp/Components/BuildImageShow.razor.cs
@@ -0,0 +1,15 @@
+﻿using KubeLife.Kubernetes.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace KubeLife.BlazorApp.Components
+{
+    public partial class BuildImageShow
+    {
+        //[Parameter]
+        //public string StatusPhase { get; set; }
+        //[Parameter]
+        //public string BuildName { get; set; }
+        [Parameter]
+        public KubeBuildModel BuildData { get; set; }
+    }
+}
