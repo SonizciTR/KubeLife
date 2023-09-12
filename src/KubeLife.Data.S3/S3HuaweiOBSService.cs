e3ae0e13d1a29a7386fbad92e8929f1a67598a95
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2023-09-12 19:49:29 +0300

    Playing around with huawei s3 storage

---
+++ src/KubeLife.Data.S3/S3HuaweiOBSService.cs
@@ -0,0 +1,45 @@
+﻿using KubeLife.Core.Models;
+using KubeLife.DataDomain;
+using KubeLife.DataDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OBS;
+using OBS.Model;
+
+namespace KubeLife.Data.S3
+{
+    public class S3HuaweiOBSService : IS3Service
+    {
+        private bool isInitialized = false;
+        private ObsClient obsClient = null;
+
+        public async Task<KubeLifeResult<string>> Initialize(string endpoint, string accessKey, string secretKey, bool useHttps = true)
+        {
+            obsClient = new ObsClient(accessKey, secretKey, endpoint);
+
+            return new KubeLifeResult<string>(true, "Success");
+        }
+
+        public async Task<KubeLifeResult<List<S3Bucket>>> GetBuckets()
+        {
+            var target = new List<S3Bucket>();
+
+            ListBucketsRequest request = new ListBucketsRequest();
+            ListBucketsResponse response = obsClient.ListBuckets(request);
+            foreach (ObsBucket bucket in response.Buckets)
+            {
+                var tmp = new S3Bucket
+                {
+                    Name = bucket.BucketName,
+                    CreatedDate = bucket.CreationDate ?? DateTime.MinValue,
+                };
+                target.Add(tmp);
+            }
+
+            return new KubeLifeResult<List<S3Bucket>>(target);
+        }
+    }
+}
--- unittests/KubeLife.DataDomain.Tests/DataGeneratorTest.cs
+++ unittests/KubeLife.DataDomain.Tests/DataGeneratorTest.cs
@@ -13,17 +13,11 @@ namespace KubeLife.DataDomain.Tests
 
     public class DataGeneratorTest
     {
+        
+
         [Fact]
         public async Task DataGenerator_DataAddingtoEmptyClass_FilliningEachProperty()
         {
-            /*
-                
-            */
-
-            IS3Service s3 = new S3MinioService();
-            var rConn = await s3.Initialize("10.11.91.61", "BB690C1D2764F9AA0586", "rbmAprPOitJqfRM2erSzWH6d8scAAAGKJ2T5qmXg", false);
-            var rBuck = await s3.GetBuckets();
-
             var data = DataGenerator.GenerateData<TestClass1>();
             Assert.NotNull(data.Name);
         }
