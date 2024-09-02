c6a0bc6e2bc483492de252e4fcc089e0d73ab40d
OZAN UYSAL - Veri ve Yapay Zeka Direktorlugu <O_UYSAL4@thynet.thy.com>
2024-09-02 13:23:50 +0300

    UX changes 2

---
+++ src/KubeLife.BlazorApp/Components/CronJobImageShow.razor.cs
@@ -0,0 +1,12 @@
+﻿using KubeLife.Domain.Models;
+using KubeLife.Kubernetes.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace KubeLife.BlazorApp.Components
+{
+    public partial class CronJobImageShow
+    {
+        [Parameter]
+        public KubeCronJobModelView CronJobDetail { get; set; }
+    }
+}
